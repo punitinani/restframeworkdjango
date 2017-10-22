b360c122a5b8304aca23d341d30926a5ba141470
punit <punitinani1@gmail.com>
2017-10-22 12:14:34 +0530

    Serializer (using)

--- restfremework/show/views.py
+++ restfremework/show/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import employee
+from .serializers import employeeSerializer
+
+
+class employeeList(APIView):
+    def get(self, request):
+        emp = employee.objects.all()
+        serial = employeeSerializer(emp, many=True)
+        return Response(serial.data)
+
+    def post(self, request):
+        pass
