a4eff5851a508ec0f61e88dbd487f67fbed02343
punit <punitinani1@gmail.com>
2017-10-22 12:17:13 +0530

    url

--- restfremework/restfremework/urls.py
+++ restfremework/restfremework/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from show.views import employeeList
+
+import restfremework
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^emplist/', employeeList.as_view()),
 ]
